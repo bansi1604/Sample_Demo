c26a0c902cbbcadf832adf8e6995e67bf192e911
Bansi <Bansi@LAPTOP-H89L9JLT>
2025-09-22 09:47:07 +0530

    Commit conflict

--- src/test/java/Practice/GitBranchPrac.java
+++ src/test/java/Practice/GitBranchPrac.java
@@ -8,6 +8,6 @@ public class GitBranchPrac {
 	public void GitBranchPrac()
 	{
 		System.out.println("This is  paushed through branch");
-		System.out.println("Paushed from the ATE workspace for GIT conflict");
+		System.out.println("Paushed from the ATE workspace for GIT practice conflict");
 	}
 }
